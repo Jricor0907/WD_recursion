ce52a59e23305b7488fa87f9e2e224d351e610c5
Juan Rico <juanricor23@gmail.com>
2021-09-28 10:58:15 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+"use strict"
+/* 01-----------------------Power---------------------------------------------------------------------
+Write a function called power which accepts a base and an exponent. 
+The function should return the power of the base to the exponent. 
+This function should mimic the functionality of Math.pow()  
+        - do not worry about negative bases and exponents.
+*/
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(){
+    
+}
+
+/* 02-----------------------productOfArray---------------------------------------------------------------------
+Write a function called 'productOfArray' which takes in an array of numbers and 
+returns the product of them all.
+*/
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
+
+function productOfArray(){
+
+}
+
+/* 03-----------------------reverse---------------------------------------------------------------------
+Write a recursive function called 'reverse' which accepts a string and returns 
+a new string in reverse.
+*/
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
+
+function reverse(){
+    // add whatever parameters you deem necessary - good luck!
+}
+
+/* 04-----------------------isPalindrome---------------------------------------------------------------------
+Write a recursive function called 'isPalindrome' which returns true if the string 
+passed to it is a palindrome (reads the same forward and backward). 
+Otherwise it returns false.
+*/
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(){
+    // add whatever parameters you deem necessary - good luck!
+}

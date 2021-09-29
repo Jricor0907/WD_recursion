ee54436a2877f12da077142f3f917ea0cf8641f7
Juan Rico <juanricor23@gmail.com>
2021-09-29 09:11:08 -0700

    reverse

--- main.js
+++ main.js
@@ -8,14 +8,14 @@ This function should mimic the functionality of Math.pow()
 // power(2,0) // 1
 // power(2,2) // 4
 // power(2,4) // 16
-  
-const productOfArray = (numbers) => {
-    if (numbers.length === 0) return 1;
-  
-    const leadNumber = numbers.shift();
-    return leadNumber * productOfArray(numbers);
-  };
-  console.log(productOfArray([1, 2, 3, 10]));
+
+function power(num, times) {
+    const MathPow = Math.pow(num, times);
+    return MathPow;
+  }
+console.log(power(2, 0));
+console.log(power(2, 2));
+console.log(power(2, 4));
 
 /* 02-----------------------productOfArray---------------------------------------------------------------------
 Write a function called 'productOfArray' which takes in an array of numbers and 
@@ -30,17 +30,17 @@ function productOfArray(num) {
       total = total * i;
     }
     return total;
-  }
-  console.log(productOfArray([1, 2, 3]));
-  console.log(productOfArray([1, 2, 3, 10]));
+}
+console.log(productOfArray([1, 2, 3]));
+console.log(productOfArray([1, 2, 3, 10]));
   
   function productOfArray2(array) {
     if (array.length === 0) return 1;
   
     return array.shift() * productOfArray2(array);
-  }
-  console.log(productOfArray2([1, 2, 3]));
-  console.log(productOfArray2([1, 2, 3, 10]));
+}
+console.log(productOfArray2([1, 2, 3]));
+console.log(productOfArray2([1, 2, 3, 10]));
   
 
 /* 03-----------------------reverse---------------------------------------------------------------------
@@ -54,7 +54,7 @@ function reverse(str) {
     return str.split("").reverse().join("");
     // add whatever parameters you deem necessary - good luck!
   }
-  console.log(reverse("awesome"));
+console.log(reverse("awesome"));
   
   function reverse2(str) {
     if (str === "") {
@@ -62,8 +62,8 @@ function reverse(str) {
     } else {
       return reverse2(str.substr(1)) + str.charAt(0);
     }
-  }
-  console.log(reverse2("awesome"));
+   }
+console.log(reverse2("awesome"));
   
 
 /* 04-----------------------isPalindrome---------------------------------------------------------------------
@@ -82,8 +82,8 @@ function isPalindrome(str) {
     return str === reverse(str);
   }
   
-  console.log(isPalindrome('awesome')); 
-  console.log(isPalindrome('foobar')); 
-  console.log(isPalindrome('tacocat'));
-  console.log(isPalindrome('amanaplanacanalpanama')); 
-  console.log(isPalindrome('amanaplanacanalpandemonium'));
+console.log(isPalindrome('awesome')); 
+console.log(isPalindrome('foobar')); 
+console.log(isPalindrome('tacocat'));
+console.log(isPalindrome('amanaplanacanalpanama')); 
+console.log(isPalindrome('amanaplanacanalpandemonium'));
